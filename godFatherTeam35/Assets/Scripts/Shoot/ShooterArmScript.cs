5ac1309803be9785041c718b5e830fafc68f025f
BVerstaen <bverstaen04@gmail.com>
2024-09-10 20:56:44 +0200

    Start prototype arm physics

--- godFatherTeam35/Assets/Scripts/Shoot/ShooterArmScript.cs
+++ godFatherTeam35/Assets/Scripts/Shoot/ShooterArmScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ShooterArmScript : MonoBehaviour
 {
     Coroutine _followMouse;
+
+    [SerializeField] Rigidbody2D _rb;
+
     private void Awake()
     {
         _followMouse = StartCoroutine(FollowMouse());
@@ -13,10 +16,16 @@ public class ShooterArmScript : MonoBehaviour
     {
         while (!GameManager.Instance.GameEnded)
         {
+            /* Version avec transform position (obselete)
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = Camera.main.nearClipPlane;
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
             transform.position = mousePosition;
+            */
+
+            Vector3 mousePo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePo.z = 0;
+            _rb.MovePosition(mousePo);
 
             yield return null;
         }
