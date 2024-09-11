8c5aad8932571e8c755ceac791b350bc0e49915d
BVerstaen <bverstaen04@gmail.com>
2024-09-11 19:54:51 +0200

    Fini moving shadow
    Add :
    Prefab & variant for moving shadow
    
    Modified :
    Finish moving shadow
    
    Remove:
    Delete this

--- godFatherTeam35/Assets/Scripts/Shoot/MovingShadowScript.cs
+++ godFatherTeam35/Assets/Scripts/Shoot/MovingShadowScript.cs
@@ -8,18 +8,29 @@ public class MovingShadowScript : MonoBehaviour
     [SerializeField] private Transform _firstPoint;
     [SerializeField] private Transform _secondPoint;
     [SerializeField] private Transform _shadowTransform;
+    [SerializeField] private float _ZPosition;
     [Space(5)]
+    [SerializeField] private _animationMode _WalkingMode;
     [SerializeField] private float _duration;
     [SerializeField] private float _stepSpeed;
     [SerializeField] private float _stepAmplitude;
+    [SerializeField] private float _TimeBetweenCycles;
 
     SpriteRenderer _shadowSR;
     Coroutine _walkingCoroutine;
 
+    private enum _animationMode
+    {
+        BackAndForth,
+        BackAndForth_inverted,
+        Looping,
+        Looping_inverted
+    }
+
     private void Awake()
     {
-        _walkingCoroutine = StartCoroutine(WalkingCoroutine());
         _shadowSR = _shadowTransform.gameObject.GetComponent<SpriteRenderer>();
+        _walkingCoroutine = StartCoroutine(WalkingCoroutine());
     }
 
     IEnumerator WalkingCoroutine()
@@ -27,26 +38,69 @@ public class MovingShadowScript : MonoBehaviour
         float timeElapsed = 0;
         Vector3 startingPosition = Vector3.zero;
         Vector3 endingPosition = _secondPoint.localPosition;
+
+        //Initialization of walking mode
+        switch (_WalkingMode)
+        {
+            case _animationMode.BackAndForth:
+            case _animationMode.Looping:
+                timeElapsed = 0;
+                startingPosition = Vector3.zero;
+                endingPosition = _secondPoint.localPosition;
+                break;
+
+            case _animationMode.BackAndForth_inverted:
+            case _animationMode.Looping_inverted:
+                startingPosition = _secondPoint.localPosition;
+                endingPosition = Vector3.zero;
+                _shadowSR.flipX = true;
+                break;
+        }
+
+
+
         while (true)
         {
             timeElapsed = 0;
-
+            //Walk animation
             while (timeElapsed < _duration)
             {
-                Vector2 lerpPosition;
+                Vector3 lerpPosition;
                 lerpPosition.x = Mathf.Lerp(startingPosition.x, endingPosition.x, timeElapsed / _duration);
                 lerpPosition.y = Mathf.PingPong(timeElapsed * _stepSpeed, _stepAmplitude);
+                lerpPosition.z = _ZPosition;
                 _shadowTransform.localPosition = lerpPosition;
 
                 timeElapsed += Time.deltaTime;
                 yield return null;
             }
 
-            _shadowSR.flipX = !_shadowSR.flipX;
+            //Change depending on walk mode
+            switch (_WalkingMode)
+            {
+                case _animationMode.BackAndForth_inverted:
+                case _animationMode.BackAndForth:
+                    Vector3 keepPosition = endingPosition;
+                    endingPosition = startingPosition;
+                    startingPosition = keepPosition;
+                    _shadowSR.flipX = !_shadowSR.flipX;
+                    break;
 
-            Vector3 keepPosition = endingPosition;
-            endingPosition = startingPosition;
-            startingPosition = keepPosition;
+                case _animationMode.Looping:
+                    timeElapsed = 0;
+                    startingPosition = Vector3.zero;
+                    endingPosition = _secondPoint.localPosition;
+                    break;
+                case _animationMode.Looping_inverted:
+                    startingPosition = _secondPoint.localPosition;
+                    endingPosition = Vector3.zero;
+                    break;
+            }
+
+            //Wait if necessary
+            _shadowSR.gameObject.SetActive(false);
+            yield return new WaitForSeconds(_TimeBetweenCycles);
+            _shadowSR.gameObject.SetActive(true);
         }
 
     }
