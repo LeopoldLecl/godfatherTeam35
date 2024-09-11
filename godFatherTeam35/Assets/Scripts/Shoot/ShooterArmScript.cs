be88723757af089c5c3390b7a4831b4de32c4656
BVerstaen <bverstaen04@gmail.com>
2024-09-11 09:17:44 +0200

    Limit Cam scrolling in Placement mode & Finish arm
    Modified :
    Placement Scene : limit cam scrolling
    Shoot Scene : modify hinge joint & arm position
    ShooterArmScript : Add hand rotation

--- godFatherTeam35/Assets/Scripts/Shoot/ShooterArmScript.cs
+++ godFatherTeam35/Assets/Scripts/Shoot/ShooterArmScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ShooterArmScript : MonoBehaviour
 {
-    Coroutine _followMouse;
-
+    [Header("References")]
     [SerializeField] Rigidbody2D _rb;
+    [SerializeField] Transform _armTransform;
+    [SerializeField] Transform _rotationPoint;
+
+    Coroutine _followMouse;
 
     private void Awake()
     {
@@ -26,6 +29,7 @@ public class ShooterArmScript : MonoBehaviour
             Vector3 mousePo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePo.z = 0;
             _rb.MovePosition(mousePo);
+            _rb.SetRotation(_armTransform.eulerAngles.z); 
 
             yield return null;
         }
